a9cbbaa6fd248d869238adacdfc2ab37a24132f2
suji-31 <sujiruthi3119@gmail.com>
2025-01-01 20:02:22 +0530

    Assign

---
+++ operations (assignment).py
@@ -0,0 +1,34 @@
+#Task 1
+num1=10
+name="john"
+is_python=True
+print(type(num1))
+print(type(name))
+print(type(is_python))
+
+#Task2
+s=('hello'+name)
+print(s)
+print(len(s))
+
+#Task3
+print(num1+5)
+print(3-num1)
+print(num1*2)
+print(num1//3)
+print(num1%4)
+print(num1==15)
+print(num1>8)
+print(num1!=10)
+
+#Task4
+user_num=str(input('enter the value:'))
+print(user_num)
+print(type(user_num))
+
+#Task5
+is_true=True
+is_false=False
+print(is_true and is_false)
+print(is_true or is_false)
+print(not(is_true and is_false))
